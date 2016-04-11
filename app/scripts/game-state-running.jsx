d020093c93c7a633fbc40abbbb14b89619b83008
humancatfood <janborchers09@googlemail.com>
2016-04-11 05:26:54 +0100

    [~] factored grid into its own component so that it can react better to window resizing

--- app/scripts/game-state-running.jsx
+++ app/scripts/game-state-running.jsx
@@ -2,13 +2,13 @@
   'use strict';
 
   var React = require('react');
-  var calculateGrid = require('./utils/calculate-grid.js');
+  var GameGrid = require('./grid.jsx');
 
   module.exports = React.createClass({
 
     getInitialState: function () {
       return {
-        display: null
+        grid: null
       };
     },
 
@@ -22,131 +22,21 @@
 
         setTimeout (function () {
 
-          var grid = calculateGrid($img);
-          var display = [];
-          var src = $img.attr('src');
-          var row, pieceStyle ;
-          var id = 0;
-
-          var positionerStyle = {
-            width: grid.pieces.width,
-            height: grid.pieces.height
-          };
-
-          for (var y = 0; y < grid.height; y += 1)
-          {
-            row = [];
-            for (var x = 0; x < grid.width; x += 1)
-            {
-              pieceStyle = {
-                position: 'absolute',
-                width: grid.image.width,
-                height: grid.image.height,
-                left: x * grid.pieces.width * -1,
-                top: y * grid.pieces.height * -1
-              };
-              id += 1;
-
-              row.push((
-                <td className="piece-positioner" data-id={id} style={positionerStyle}>
-                  <div className="piece-wrapper animated" data-id={id} >
-                    <img src={src} className="piece" style={pieceStyle} />
-                  </div>
-                </td>
-              ));
-            }
-
-            display.push((
-              <tr>
-                {row}
-              </tr>
-            ))
-          }
-
-
           that.setState({
-            display: display
+            showGrid: true
           });
-
 
           $img.addClass('invisible');
-
-          $('.piece-positioner').each(function () {
-
-            var $piecePositioner = $(this);
-            var $piece = $piecePositioner.find('.piece-wrapper');
-
-            setTimeout(function () {
-
-              $piece
-                .offset({
-                  top: $stage.height() - (grid.pieces.height + Math.random() * grid.pieces.height * 2),
-                  left: grid.pieces.width + Math.random() * ($stage.width() - grid.pieces.width * 2)
-                })
-                .draggable({
-                  stack: '.piece-wrapper',
-                  snap: '.piece-positioner',
-                  snapMode: 'inner',
-                  snapTolerance: Math.max(grid.pieces.width, grid.pieces.height) / 3,
-                  containment: $('.stage'),
-                  classes: {
-                    "ui-draggable": "test",
-                    "ui-draggable-dragging": "highlight"
-                  },
-                  start: function () {
-                    $piece.removeClass('animated');
-                  },
-                  stop: function () {
-                    $piece.addClass('animated');
-                  }
-                });
-
-                $piecePositioner.droppable({
-                  tolerance: "intersect",
-                   drop: function( event, ui ) {
-                     var $this = $(this);
-                     var $piece = $(ui.draggable);
-
-                     $piece.offset($this.offset());
-
-                     var $currentPiece = $this.data('currentPiece');
-                     if ($currentPiece && $currentPiece !== $piece)
-                     {
-                       $currentPiece
-                        .offset({
-                          top: $stage.height() - (grid.pieces.height + Math.random() * grid.pieces.height * 2),
-                          left: grid.pieces.width + Math.random() * ($stage.width() - grid.pieces.width * 2)
-                        });
-                     }
-
-                     if ($piece.data('id') === $this.data('id'))
-                     {
-                       $this.droppable('destroy');
-                       $piece.draggable('destroy');
-                       $this.addClass('done');
-                     }
-
-                     $this.data('currentPiece', $piece);
-
-                     console.log(checkIfDone());
-
-                   },
-                   out: function (event, ui) {
-                     if ($(this).data('currentPiece') && $(this).data('currentPiece').data('id') === $(ui.draggable).data('id'))
-                     {
-                       $(this).data('currentPiece', null);
-                     }
-                   }
-                });
-
-            }, Math.random() * 500);
-
-          });
 
         }, 500);
 
 
       });
+
+    },
+
+    setupGameLogic: function () {
+      console.log("setupGameLogic");
 
     },
 
@@ -160,13 +50,17 @@
     },
 
     renderGrid: function () {
-      return (
-        <table className="game-grid">
-          <tbody>
-            {this.state.display}
-          </tbody>
-        </table>
-      );
+      if (this.state.showGrid)
+      {
+        console.log("render");
+        return (
+          <GameGrid ref="grid" $img={$(this.refs.img)} width={4} height={3} onLoad={this.setupGameLogic} />
+        );
+      }
+      else
+      {
+        return null;
+      }
     },
 
     renderStage: function () {
@@ -191,17 +85,18 @@
 
   });
 
+
   function checkIfDone()
   {
     var allCorrectlyPlaced;
     $('.piece-positioner').each(function () {
 
-      allCorrectlyPlaced = !!$(this).data('currentPiece') && $(this).data('currentPiece').data('id') === $(this).data('id');
-      return allCorrectlyPlaced;
+      allCorrectlyPlaced = $(this).data('currentPiece') && $(this).data('currentPiece').data('id') === $(this).data('id');
+      return !!allCorrectlyPlaced;
 
     });
 
-    return allCorrectlyPlaced;
+    return !!allCorrectlyPlaced;
   }
 
 }(window.jQuery));
---
+++ app/scripts/grid.jsx
@@ -0,0 +1,100 @@
+(function ($) {
+  'use strict';
+
+  var React = require('react');
+
+  module.exports = React.createClass({
+
+    getInitialState: function () {
+      var $img = this.props.$img;
+
+      return {
+        imgSrc: this.props.$img.attr('src'),
+        imgWidth: this.props.$img.width(),
+        imgHeight: this.props.$img.height()
+      };
+
+    },
+
+    componentDidMount: function () {
+
+
+      this.props.onLoad && this.props.onLoad(this.refs.table);
+
+      var that = this;
+      $(window).resize(function () {
+        that.setState({
+          imgWidth: that.props.$img.width(),
+          imgHeight: that.props.$img.height()
+        })
+      });
+
+    },
+
+    render: function () {
+
+      var imgSrc = this.state.imgSrc;
+      var imgWidth = this.state.imgWidth;
+      var imgHeight = this.state.imgHeight;
+
+      var gridWidth = this.props.width;
+      var gridHeight = this.props.height;
+
+
+      // TODO: do something more clever here to make the pieces more square
+      var pieceWidth = imgWidth / gridWidth;
+      var pieceHeight = imgHeight / gridHeight;
+
+
+      var runningID = 0;
+
+      var cellStyle = {
+        width: pieceWidth,
+        height: pieceHeight
+      };
+
+      var grid = [];
+      for (var y = 0; y < gridHeight; y += 1)
+      {
+        var row = [];
+
+        for (var x = 0; x < gridWidth; x += 1)
+        {
+          var pieceStyle = {
+            width: imgWidth,
+            height: imgHeight,
+            left: x * pieceWidth * -1,
+            top: y * pieceHeight * -1
+          };
+          runningID += 1;
+
+          row.push((
+            <td className="piece-positioner" data-id={runningID} style={cellStyle}>
+              <div className="piece-wrapper animated" data-id={runningID} >
+                <img src={imgSrc} className="piece" style={pieceStyle} />
+              </div>
+            </td>
+          ));
+
+        }
+
+        grid.push((
+          <tr>
+            {row}
+          </tr>
+        ))
+      }
+
+      return (
+        <table ref="table" className="game-grid">
+          <tbody>
+            {grid}
+          </tbody>
+        </table>
+      );
+    }
+
+  });
+
+
+}(window.jQuery));
