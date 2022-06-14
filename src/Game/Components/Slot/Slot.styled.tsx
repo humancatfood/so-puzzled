1c6c097b2e1b252637c858e8b7d71f8d97c30af0
Jan Borchers <humancatfood@users.noreply.github.com>
2022-06-14 14:07:42 +0200

    fix: make drop targets work on firefox

--- src/Game/Components/Slot/Slot.styled.tsx
+++ src/Game/Components/Slot/Slot.styled.tsx
@@ -1,7 +1,10 @@
-import styled from 'styled-components'
+import styled, { CSSProperties } from 'styled-components'
 
 export const SlotWrapper = styled.div((props: { isHighlighted: boolean }) => ({
-  display: 'block',
-  height: '100%',
-  outline: props.isHighlighted ? '3px solid rebeccapurple' : 'none',
+  outline: props.isHighlighted ? '3px solid rebeccapurple' : 'none', // TODO: make this show just the corners, so we get an outline without cutting across the noses and holes
+  position: 'absolute' as CSSProperties['position'],
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
 }))
